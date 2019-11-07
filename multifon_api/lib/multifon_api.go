01acfa73336649df957e9211fc7abf28cd9ecc83
lucky <>
2019-11-07 18:10:32 +0300

    init

---
+++ multifon_api/lib/multifon_api.go
@@ -0,0 +1,252 @@
+package multifonapi
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	MULTIFON_API_URL = "https://sm.megafon.ru/sm/client/"
+	EMOTION_API_URL  = "https://emotion.megalabs.ru/sm/client/"
+	DEFAULT_API_URL  = MULTIFON_API_URL
+
+	ROUTING_GSM     = 0
+	ROUTING_SIP     = 1
+	ROUTING_SIP_GSM = 2
+
+	STATUS_ACTIVE  = 0
+	STATUS_BLOCKED = 1
+
+	strUndefined = "undefined"
+)
+
+var ROUTING_DESCRIPTION_MAP = map[int]string{
+	ROUTING_GSM:     "GSM",
+	ROUTING_SIP:     "SIP",
+	ROUTING_SIP_GSM: "SIP+GSM",
+}
+
+type Response interface {
+	ResultError() error
+}
+
+type ResponseResult struct {
+	XMLName xml.Name `xml:"response"`
+	Result  struct {
+		Code        int    `xml:"code"`
+		Description string `xml:"description"`
+	} `xml:"result"`
+}
+
+func (s *ResponseResult) ResultError() error {
+	if s.Result.Code != http.StatusOK {
+		return errors.New(s.Result.Description)
+	}
+	return nil
+}
+
+type ResponseBalance struct {
+	ResponseResult
+	Balance float64 `xml:"balance"`
+}
+
+type ResponseRouting struct {
+	ResponseResult
+	Routing int `xml:"routing"`
+}
+
+func (s *ResponseRouting) Description() string {
+	if v, ok := ROUTING_DESCRIPTION_MAP[s.Routing]; ok {
+		return v
+	}
+	return strUndefined
+}
+
+type ResponseStatus struct {
+	ResponseResult
+	Status  int    `xml:"status"`
+	Expires string `xml:"expires"` // [U]Int?
+}
+
+func (s *ResponseStatus) Description() string {
+	switch s.Status {
+	case STATUS_ACTIVE:
+		return "active"
+	case STATUS_BLOCKED:
+		return "blocked"
+	default:
+		return strUndefined
+	}
+}
+
+type ResponseProfile struct {
+	ResponseResult
+	MSISDN string `xml:"msisdn"`
+}
+
+type ResponseLines struct {
+	ResponseResult
+	Lines int `xml:"ParallelCallsSipOut"`
+}
+
+func newSetFailedError(key string, value, currentValue interface{}) error {
+	return fmt.Errorf(
+		"Failed to set %s, invalid value: %v (current: %v)",
+		key,
+		value,
+		currentValue,
+	)
+}
+
+type Client struct {
+	httpClient *http.Client
+	login      string
+	password   string
+	apiUrl     string
+}
+
+func (s *Client) Request(
+	urlPath string,
+	params map[string]string,
+) ([]byte, error) {
+	reqUrl, err := urlJoin(s.apiUrl, urlPath)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodPost, reqUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	q := req.URL.Query()
+	q.Add("login", s.login)
+	q.Add("password", s.password)
+	if params != nil {
+		for k, v := range params {
+			q.Add(k, v)
+		}
+	}
+	req.URL.RawQuery = q.Encode()
+	resp, err := s.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= http.StatusBadRequest && resp.StatusCode < 600 {
+		return nil, errors.New(resp.Status)
+	}
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func (s *Client) Do(
+	urlPath string,
+	params map[string]string,
+	data Response,
+) error {
+	buf, err := s.Request(urlPath, params)
+	if err != nil {
+		return err
+	}
+	if err := xml.Unmarshal(buf, data); err != nil {
+		return err
+	}
+	if err := data.ResultError(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Client) GetBalance() (*ResponseBalance, error) {
+	data := &ResponseBalance{}
+	return data, s.Do("balance", nil, data)
+}
+
+func (s *Client) GetRouting() (*ResponseRouting, error) {
+	data := &ResponseRouting{}
+	return data, s.Do("routing", nil, data)
+}
+
+/*
+ROUTING_GSM, ROUTING_SIP, ROUTING_SIP_GSM
+*/
+func (s *Client) SetRouting(routing int) (*ResponseRouting, error) {
+	k := "routing"
+	v := -1
+	data := &ResponseRouting{Routing: v}
+	err := s.Do(k, map[string]string{k: strconv.Itoa(routing)}, data)
+	if err == nil && data.Routing != v {
+		err = newSetFailedError(k, routing, data.Routing)
+	}
+	return data, err
+}
+
+func (s *Client) GetStatus() (*ResponseStatus, error) {
+	data := &ResponseStatus{}
+	return data, s.Do("status", nil, data)
+}
+
+/*
+outdated?
+*/
+func (s *Client) GetProfile() (*ResponseProfile, error) {
+	data := &ResponseProfile{}
+	return data, s.Do("profile", nil, data)
+}
+
+func (s *Client) GetLines() (*ResponseLines, error) {
+	data := &ResponseLines{}
+	return data, s.Do("lines", nil, data)
+}
+
+/*
+2 .. 20
+*/
+func (s *Client) SetLines(n int) (*ResponseLines, error) {
+	k := "lines"
+	v := -1
+	data := &ResponseLines{Lines: v}
+	err := s.Do(k, map[string]string{k: strconv.Itoa(n)}, data)
+	if err == nil && data.Lines != v {
+		err = newSetFailedError(k, n, data.Lines)
+	}
+	return data, err
+}
+
+/*
+min 8, max 20, mixed case, digits
+can return error and.. change password on server?
+*/
+func (s *Client) SetPassword(password string) (*ResponseResult, error) {
+	data := &ResponseResult{}
+	err := s.Do("password", map[string]string{"new_password": password}, data)
+	if err == nil {
+		s.password = password
+	}
+	return data, err
+}
+
+func NewClient(
+	login, password, apiUrl string,
+	httpClient *http.Client,
+) *Client {
+	if apiUrl == "" {
+		apiUrl = DEFAULT_API_URL
+	}
+	if httpClient == nil {
+		httpClient = &http.Client{Timeout: 30 * time.Second}
+	}
+	return &Client{
+		httpClient: httpClient,
+		login:      login,
+		password:   password,
+		apiUrl:     apiUrl,
+	}
+}
---
+++ multifon_api/main.go
@@ -0,0 +1,265 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+
+	multifonapi "./lib"
+)
+
+const (
+	Version = "0.0.1"
+
+	API_MULTIFON = "multifon"
+	API_EMOTION  = "emotion"
+
+	COMMAND_BALANCE      = "balance"
+	COMMAND_GET_ROUTING  = "get-routing"
+	COMMAND_SET_ROUTING  = "set-routing"
+	COMMAND_STATUS       = "status"
+	COMMAND_PROFILE      = "profile"
+	COMMAND_GET_LINES    = "get-lines"
+	COMMAND_SET_LINES    = "set-lines"
+	COMMAND_SET_PASSWORD = "set-password"
+
+	EX_OK      = 0
+	EX_ERR     = 1
+	EX_ARG_ERR = 2 // golang flag error exit code
+)
+
+var (
+	API_URL_MAP = map[string]string{
+		API_MULTIFON: multifonapi.MULTIFON_API_URL,
+		API_EMOTION:  multifonapi.EMOTION_API_URL,
+	}
+
+	AVAILABLE_COMMANDS = [...]string{
+		COMMAND_BALANCE,
+		COMMAND_GET_ROUTING,
+		COMMAND_SET_ROUTING,
+		COMMAND_STATUS,
+		COMMAND_PROFILE,
+		COMMAND_GET_LINES,
+		COMMAND_SET_LINES,
+		COMMAND_SET_PASSWORD,
+	}
+
+	Login      string
+	Password   string
+	API        string
+	Command    string
+	CommandArg interface{}
+
+	versionFlagName  = "V"
+	loginFlagName    = "login"
+	passwordFlagName = "password"
+	apiFlagName      = "api"
+
+	commandMetaVar    = "COMMAND"
+	commandArgMetaVar = "COMMAND_ARGUMENT"
+)
+
+func getDefaultAPI() string {
+	for k, v := range API_URL_MAP {
+		if v == multifonapi.DEFAULT_API_URL {
+			return k
+		}
+	}
+	return ""
+}
+
+func getAPIs() []string {
+	res := make([]string, len(API_URL_MAP))
+	i := 0
+	for k := range API_URL_MAP {
+		res[i] = k
+		i++
+	}
+	return res
+}
+
+func getRoutingDescriptions() []string {
+	res := make([]string, len(multifonapi.ROUTING_DESCRIPTION_MAP))
+	i := 0
+	for _, v := range multifonapi.ROUTING_DESCRIPTION_MAP {
+		res[i] = v
+		i++
+	}
+	return res
+}
+
+func getRoutingByDescription(s string) int {
+	for k, v := range multifonapi.ROUTING_DESCRIPTION_MAP {
+		if v == s {
+			return k
+		}
+	}
+	return -1
+}
+
+func printUsage() {
+	name := path.Base(os.Args[0])
+	helpFlagName := "h"
+	passwordMetaVar := "PASSWORD"
+	apiMetaVar := "API"
+	apis := getAPIs()
+	sort.Strings(apis)
+	routingDescriptions := getRoutingDescriptions()
+	sort.Strings(routingDescriptions)
+	choicesDelimiter := " | "
+	fmt.Fprintf(
+		flag.CommandLine.Output(),
+		"%s [-%s] [-%s] -%s <LOGIN> -%s <%s> [-%s <%s>] \n"+
+			"%s <%s> [<%s>]\n\n"+
+			"[-%s] * Print help and exit\n"+
+			"[-%s] * Print version and exit\n\n"+
+			"%s:\n"+
+			"  { %s } (default: %s)\n\n"+
+			"%s:\n"+
+			"  { %s }\n\n"+
+			"%s:\n"+
+			"  %s { %s }\n"+
+			"  %s NUMBER (2 .. 20)\n"+
+			"  %s NEW_%s (min 8, max 20, mixed case, digits)\n",
+		name, helpFlagName, versionFlagName, loginFlagName, passwordFlagName,
+		passwordMetaVar, apiFlagName, apiMetaVar,
+		strings.Repeat(" ", len(name)), commandMetaVar, commandArgMetaVar,
+		helpFlagName, versionFlagName, apiMetaVar,
+		strings.Join(apis, choicesDelimiter), getDefaultAPI(), commandMetaVar,
+		strings.Join(AVAILABLE_COMMANDS[:], choicesDelimiter),
+		commandArgMetaVar, COMMAND_SET_ROUTING,
+		strings.Join(routingDescriptions, choicesDelimiter), COMMAND_SET_LINES,
+		COMMAND_SET_PASSWORD, passwordMetaVar,
+	)
+}
+
+func fatalParseArgs(k, v string) {
+	fmt.Fprintf(os.Stderr, "failed to parse argument %s: \"%s\"\n", k, v)
+	os.Exit(EX_ARG_ERR)
+}
+
+func flagNameToFlag(s string) string {
+	return fmt.Sprint("-", s)
+}
+
+func fatalIfEmptyArg(desc, val string) {
+	if val == "" {
+		fatalParseArgs(desc, val)
+	}
+}
+
+func parseAPI() {
+	api, ok := API_URL_MAP[strings.ToLower(API)]
+	if !ok {
+		fatalParseArgs(flagNameToFlag(apiFlagName), API)
+	}
+	API = api
+}
+
+func parseCommand() {
+	Command = strings.ToLower(flag.Arg(0))
+	for _, v := range AVAILABLE_COMMANDS {
+		if v == Command {
+			return
+		}
+	}
+	fatalParseArgs(commandMetaVar, Command)
+}
+
+func parseCommandArg() {
+	arg := flag.Arg(1)
+	switch Command {
+	case COMMAND_SET_ROUTING:
+		routing := getRoutingByDescription(strings.ToUpper(arg))
+		if routing == -1 {
+			fatalParseArgs(commandArgMetaVar, arg)
+		}
+		CommandArg = routing
+	case COMMAND_SET_LINES:
+		n, err := strconv.Atoi(arg)
+		if err != nil {
+			fatalParseArgs(commandArgMetaVar, arg)
+		}
+		CommandArg = n
+	case COMMAND_SET_PASSWORD:
+		fatalIfEmptyArg(commandArgMetaVar, arg)
+		CommandArg = arg
+	}
+}
+
+func parseArgs() {
+	flag.Usage = printUsage
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(EX_ARG_ERR)
+	}
+	isVersion := flag.Bool(versionFlagName, false, "")
+	flag.StringVar(&Login, loginFlagName, "", "")
+	flag.StringVar(&Password, passwordFlagName, "", "")
+	flag.StringVar(&API, apiFlagName, getDefaultAPI(), "")
+	flag.Parse()
+	if *isVersion {
+		fmt.Println(Version)
+		os.Exit(EX_OK)
+	}
+	fatalIfEmptyArg(flagNameToFlag(loginFlagName), Login)
+	fatalIfEmptyArg(flagNameToFlag(passwordFlagName), Password)
+	parseAPI()
+	parseCommand()
+	parseCommandArg()
+}
+
+func main() {
+	parseArgs()
+	client := multifonapi.NewClient(Login, Password, API, nil)
+	fatalIfErr := func(e error) {
+		if e != nil {
+			fmt.Println(e)
+			os.Exit(EX_ERR)
+		}
+	}
+	strOk := "OK"
+	switch Command {
+	case COMMAND_BALANCE:
+		res, err := client.GetBalance()
+		fatalIfErr(err)
+		fmt.Println(res.Balance)
+	case COMMAND_GET_ROUTING:
+		res, err := client.GetRouting()
+		fatalIfErr(err)
+		fmt.Println(res.Description())
+	case COMMAND_SET_ROUTING:
+		_, err := client.SetRouting(CommandArg.(int))
+		fatalIfErr(err)
+		fmt.Println(strOk)
+	case COMMAND_STATUS:
+		res, err := client.GetStatus()
+		fatalIfErr(err)
+		val := res.Description()
+		if res.Expires != "" {
+			val = fmt.Sprint(val, "|", res.Expires)
+		}
+		fmt.Println(val)
+	case COMMAND_PROFILE:
+		res, err := client.GetProfile()
+		fatalIfErr(err)
+		fmt.Println(res.MSISDN)
+	case COMMAND_GET_LINES:
+		res, err := client.GetLines()
+		fatalIfErr(err)
+		fmt.Println(res.Lines)
+	case COMMAND_SET_LINES:
+		_, err := client.SetLines(CommandArg.(int))
+		fatalIfErr(err)
+		fmt.Println(strOk)
+	case COMMAND_SET_PASSWORD:
+		_, err := client.SetPassword(CommandArg.(string))
+		fatalIfErr(err)
+		fmt.Println(strOk)
+	}
+}
